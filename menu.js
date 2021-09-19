891f7cbb39035ab55d5584507a48ddea9f9c8fb9
RaCode75 <racode75@gmail.com>
2021-09-19 15:02:20 -0300

    Mobile finished

--- menu.js
+++ menu.js
@@ -6,7 +6,7 @@ const op1 = document.getElementById('group-1');
 const op2 = document.getElementById('group-2');
 const op3 = document.getElementById('group-3');
 //-------menu-principal--------------------------
-const navLabel = document.querySelectorAll('.nav-label__sub'); 
+//const navLabel = document.querySelector('.label1'); 
 
 //----------Hamburguer menu --------------
 hamburguer.addEventListener("click", ()=>{
